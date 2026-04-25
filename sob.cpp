0733ee28ea6eae0a9f28f51dd36f64a2600b90d9
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-04-25 18:23:10 +0800

    feat: Add -Isrc include path to GxxContext cxxflags

--- sob.cpp
+++ sob.cpp
@@ -11,7 +11,7 @@ struct GxxContext
     static constexpr sopho::StaticString bin_prefix{" -o "};
     static constexpr sopho::StaticString build_prefix{"build/"};
     static constexpr std::array<std::string_view, 1> ldflags{"-lX11"};
-    static constexpr std::array<std::string_view, 1> cxxflags{"-g"};
+    static constexpr std::array<std::string_view, 2> cxxflags{"-g","-Isrc"};
 };
 
 struct ClContext
